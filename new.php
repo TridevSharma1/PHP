a707a597c387e2f397d9c56f44b15f507d4061f8
TridevSharma1 <tridevx8@gmail.com>
2026-03-19 22:23:01 +0530

    PHP 2

--- new.php
+++ new.php
@@ -5,5 +5,8 @@ $name = "John";
 $age = 25;
 echo "My name is $name and I am $age years old.";
 echo "The sum of 5 and 10 is " . (5 + 10) . ".";
+echo"The value of pi is approximately " . pi() . ".";
+echo "The current date and time is " . date("Y-m-d H:i:s") . ".";
+echo "The length of the string 'Hello' is " . strlen("Hello") . ".";
 
 ?>
