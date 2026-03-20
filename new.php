21e91b436525bbc4d84294581fd7a2afe37613c1
TridevSharma1 <tridevx8@gmail.com>
2026-03-20 23:46:42 +0530

    php one line

--- new.php
+++ new.php
@@ -8,5 +8,5 @@ echo "The sum of 5 and 10 is " . (5 + 10) . ".";
 echo"The value of pi is approximately " . pi() . ".";
 echo "The current date and time is " . date("Y-m-d H:i:s") . ".";
 echo "The length of the string 'Hello' is " . strlen("Hello") . ".";
-
+echo "The square root of 16 is " . sqrt(16) . ".";
 ?>
