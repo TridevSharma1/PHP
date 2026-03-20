a808fada33dc24eb7f12c1d815ad34f39e48358b
TridevSharma1 <tridevx8@gmail.com>
2026-03-21 00:02:11 +0530

    php new

--- new.php
+++ new.php
@@ -11,4 +11,7 @@ echo "The length of the string 'Hello' is " . strlen("Hello") . ".";
 echo "The square root of 16 is " . sqrt(16) . ".";
 echo "The absolute value of -5 is " . abs(-5) . ".";
 echo "The random number between 1 and 100 is " . rand(1, 100) . ".";
+echo "The current year is " . date("Y") . ".";
+echo "The current month is " . date("F") . ".";
+echo "The current day is " . date("d") . ".";
 ?>
