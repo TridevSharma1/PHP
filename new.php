d0c81f816c096df20b0cb88d1f878450d2491f03
TridevSharma1 <tridevx8@gmail.com>
2026-03-20 23:50:06 +0530

    php 3

--- new.php
+++ new.php
@@ -9,4 +9,6 @@ echo"The value of pi is approximately " . pi() . ".";
 echo "The current date and time is " . date("Y-m-d H:i:s") . ".";
 echo "The length of the string 'Hello' is " . strlen("Hello") . ".";
 echo "The square root of 16 is " . sqrt(16) . ".";
+echo "The absolute value of -5 is " . abs(-5) . ".";
+echo "The random number between 1 and 100 is " . rand(1, 100) . ".";
 ?>
