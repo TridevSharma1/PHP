8e326edbde818046df5787850f44506ed88afba5
TridevSharma1 <tridevx8@gmail.com>
2026-05-09 15:18:28 +0530

    PHP

--- new.php
+++ new.php
@@ -277,5 +277,12 @@ echo "Enter the angle of the sector in degrees: ";
 $angle = fgets($handle);
 $area = (pi() * pow($radius, 2) * $angle) / 360;
 echo "The area of the sector of the circle is: " . $area . ".";
+// calculate the area of a segment of a circle
+echo "Enter the radius of the circle: ";
+$radius = fgets($handle);
+echo "Enter the height of the segment: ";
+$height = fgets($handle);
+$area = (pow($radius, 2) * acos(($radius - $height) / $radius)) - (($radius - $height) * sqrt((2 * $radius * $height) - pow($height, 2)));
+echo "The area of the segment of the circle is: " . $area . ".";
 
 ?>
